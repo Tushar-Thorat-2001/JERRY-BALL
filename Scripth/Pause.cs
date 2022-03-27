1a1553ce2e319326e6080be84ef0d7534815d931
Tushar-Thorat-2001 <58701931+Tushar-Thorat-2001@users.noreply.github.com>
2022-03-27 10:33:00 +0530

    Add files via upload

---
+++ Scripth/Pause.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class Pause : MonoBehaviour
+{
+    public static bool isPaused = false;
+
+    [SerializeField] GameObject pauseMenu;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else{
+                PauseGame();
+
+            }
+        }
+    }
+ public void ResumeGame()
+    {
+         pauseMenu.SetActive(false);
+         Time.timeScale = 1f;
+         isPaused = false;
+    }
+    void PauseGame()
+    {
+         pauseMenu.SetActive(true);
+         Time.timeScale = 0f;
+         isPaused = true;
+    }
+    public void  loadmenu ()
+    {
+        Time.timeScale = 0f;
+        SceneManager.LoadScene("Menu");
+    }
+
+
+  
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+        Debug.Log("QUIT!");
+    }
+ 
+ 
+}
---
+++ Scripth/unityjb/stop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stop : MonoBehaviour
+{
+   [SerializeField]
+    Vector3 v3Force;
+     [SerializeField]
+     KeyCode keyPositive;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey (keyPositive))
+           GetComponent<Rigidbody>().velocity = v3Force; 
+    }
+}
